c7566be62263eb8d4db5d6fda21ba3c594ccdd1d
DELL <DELL@DESKTOP-MC8LGET>
2017-03-10 01:40:12 +0500

    immutable in data structure

---
+++ src/Immutable.java
@@ -0,0 +1,18 @@
+/*
+ * HASSAN ARSHAD
+ */
+
+public class Immutable {
+       public static void main (String [] args)
+    {
+    
+    String s = new String ("hassan");
+   s="arshad";
+    
+    System.out.println(s);
+    
+    
+    }
+    
+    
+}
