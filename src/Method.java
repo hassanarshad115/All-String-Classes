201f2aa641449f44bc47477b87c86a1f8fdeb40c
DELL <DELL@DESKTOP-MC8LGET>
2017-03-10 01:14:21 +0500

    Square and Cube Hassan Arshad

---
+++ src/Method.java
@@ -0,0 +1,44 @@
+//24 JAVA METHODS INTRODUCTION WITH EXAMPLE
+public class Method {
+    public static void main (String []args)
+    {
+        //best programming practice h k alg alg int k sth veriables likhy
+    int num=5;
+   // int result;
+    //formal pararmeter 
+    //fuction call
+    //result= square(5);
+    
+    System.out.println(num+"the square is ="+square(5));
+    //cube k lye output
+    
+        System.out.println(num+"the cube is ="+cube(5));
+
+        
+        
+        
+        
+    }
+//square pora lna ha 
+static int square(int num)//formal parameter
+        
+{
+    //function defination
+return num*num;
+
+
+}
+
+//cube bnana ha 3 br wla
+static int cube(int num)//formal parameter
+        
+{
+    //function defination
+return num*num*num;
+
+
+}
+
+
+
+}
