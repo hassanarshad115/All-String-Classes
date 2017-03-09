4ff8c5f0b91aeb418e87ba4595c3464c48e57483
DELL <DELL@DESKTOP-MC8LGET>
2017-03-10 00:45:45 +0500

    Hassan Arshad

---
+++ src/Asigment1.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+public class Asigment1 {
+    public static void main(String []args)
+    {
+    Random sc =new Random();
+    
+    int r,h;
+    double volume,area;
+    
+    r=sc.nextInt(10)+1;
+    h=sc.nextInt(10)+1;
+    
+    volume=Math.PI*(r*r)*h;
+    area=Math.PI*2*r*h;
+    
+    System.out.println("the volume is="+volume);
+    System.out.println();
+    System.out.println("the area is="+area);
+   
+    
+    
+      }
+    
+}
