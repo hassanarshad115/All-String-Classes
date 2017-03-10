d6532eb4d40152f81be20ae3d34c2fb244f908a4
DELL <DELL@DESKTOP-MC8LGET>
2017-03-11 01:19:12 +0500

    string and substring

---
+++ src/StringandsubString.java
@@ -0,0 +1,27 @@
+
+public class StringandsubString {
+    
+    
+    public static void main(String[] args)
+    {
+    String hi="hello";
+    String h="hassan";
+    
+    
+    
+    System.out.println(hi+"\t"+h);
+    
+    //hello ma sy hel yni 123 tesry nbr tk jo ayga wo likha jayga
+    System.out.println(hi.substring(0,3));
+            
+                System.out.println(h.substring(0,4));
+
+    
+    
+    
+    
+    
+    
+    }
+    
+}
