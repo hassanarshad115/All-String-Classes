db6cb3f31cdda935f63477ac097bf0b61ccec86f
DELL <DELL@DESKTOP-MC8LGET>
2017-03-11 00:33:32 +0500

    stack and flow

---
+++ src/StackDemo.java
@@ -0,0 +1,52 @@
+public class StackDemo {
+    
+ private static final int capacity = 3;
+ int arr[] = new int[capacity];
+ int top = -1;
+
+ public void push(int pushedElement)
+ {
+     if (top < capacity - 1) 
+  {
+      top++;
+      arr[top] = pushedElement;
+      System.out.println("Element " + pushedElement+ " is pushed to Stack !");
+   printElements();
+  } else {
+   System.out.println("Stack Overflow !");
+  }
+ }
+
+ public void pop() {
+  if (top >= 0) {
+   top--;
+   System.out.println("Pop operation done !");
+  } else {
+   System.out.println("Stack Underflow !");
+  }
+ }
+
+ public void printElements() {
+  if (top >= 0) {
+   System.out.println("Elements in stack :");
+   for (int i = 0; i <= top; i++) {
+    System.out.println(arr[i]);
+   }
+  }
+ }
+
+ public static void main(String[] args) {
+  StackDemo h = new StackDemo();
+
+  h.pop();
+  h.push(23);
+  h.push(2);
+  h.push(73);
+  h.push(21);
+  h.pop();
+  h.pop();
+  h.pop();
+  h.pop();
+ }
+
+}
