921d499960068380cca3ac2edd2636f28a1e31ec
DELL <DELL@DESKTOP-MC8LGET>
2017-03-11 01:25:41 +0500

    random 0.00 to 1.0

---
+++ src/Ran.java
@@ -0,0 +1,17 @@
+
+public class Ran {
+    public static void main (String []args)
+    {
+    System.out.println(Math.random());
+    
+       // System.out.println(10*(5+Math.random()));
+
+       // System.out.println(100*Math.random());
+
+    
+    }
+    
+}
+//by default ye 0.00 sy 1.00 k drmayan chlyga
+
+//ye double ma ans return kryga
