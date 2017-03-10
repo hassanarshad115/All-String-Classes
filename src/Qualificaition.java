d9b48441b24b6d4ff282555231643399f62c327e
DELL <DELL@DESKTOP-MC8LGET>
2017-03-11 01:29:20 +0500

    with Math and sin ,cos ,tan

---
+++ src/Qualificaition.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+public class Qualificaition 
+{
+    public static void main (String [] args)
+    {
+    Random r = new Random ();
+    int n;
+    
+    n=r.nextInt(21)+20;
+    
+    System.out.println(n);
+    
+   System.out.println(Math.sin(n));
+
+     System.out.println(Math.cos(n));
+    System.out.println(Math.tan(n));
+
+    
+    
+    }
+    
+}
