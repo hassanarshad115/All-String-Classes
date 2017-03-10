fe91b7062829ef5e568c84b674680f47f2c9b091
DELL <DELL@DESKTOP-MC8LGET>
2017-03-11 00:11:25 +0500

    boolean

---
+++ src/Java1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+        
+        class Java1
+{
+    public static void main(String [] args)
+    {
+       Scanner sc=new Scanner (System.in);
+        
+        boolean a;
+        
+        
+       System.out.print("please enter the value");
+               a=sc.nextBoolean();
+        
+        System.out.println("world\t"+a+"\tsides");
+        
+        
+    }   
+        
+    }
+    
+    
+    
+    
+    
+    
